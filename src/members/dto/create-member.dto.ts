41bd597bf1b1a3ea5db601ef166fc682845700f4
arisymeon <arisymeon@protonmail.com>
2025-04-26 11:30:39 +0300

    dtos for create, edit and delete requests

---
+++ src/members/dto/create-member.dto.ts
@@ -0,0 +1,13 @@
+export class CreateMemberDto {
+    id: string;
+    last_name: string;
+    first_name: string;
+    expertise: string;
+    sector: string;
+    health_unit: string;
+    work_place: string;
+    home_place: string;
+    email: string;
+    phone: string;
+    consent: string;
+}
---
+++ src/members/dto/delete-member.dto.ts
@@ -0,0 +1,3 @@
+export class DeleteMembersDto {
+    id: string
+}
