daacb8fb63638d85a0ad44f814a847965f9dbced
arisymeon <arisymeon@protonmail.com>
2025-04-26 11:33:46 +0300

    members entity

---
+++ src/members/entities/members.entity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('members')
+export class Members {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    last_name: string;
+
+    @Column()
+    first_name: string;
+
+    @Column()
+    expertise: string;
+
+    @Column()
+    sector: string;
+
+    @Column()
+    health_unit: string;
+
+    @Column()
+    work_place: string;
+
+    @Column()
+    home_place: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    phone: string;
+
+    @Column()
+    consent: string;
+
+
+}
