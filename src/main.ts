3787b123567d8467ce5307ed5c1ab2d9dffd4a8e
arisymeon <arisymeon@protonmail.com>
2025-04-26 11:34:57 +0300

    rest config files

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow your Next.js frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+  await app.listen(4000);
+}
+bootstrap();
