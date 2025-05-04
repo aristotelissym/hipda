d929022fae592ebf5529bc200d1dac2f00f5c471
arisymeon <arisymeon@protonmail.com>
2025-05-04 11:57:41 +0300

    updated for contianer solution

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow your Next.js frontend
+    origin: 'http://localhost:3000', // Allow frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
-  await app.listen(4000);
+  await app.listen(4000, '0.0.0.0');
 }
 bootstrap();
